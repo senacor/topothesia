28f8a6e144f44d45b4c3b5b7ade86eb110598bfb
Rudisch, Florian <Florian.Rudisch@senacor.com>
2020-11-05 17:51:19 +0100

    Added document select function

--- document-selection/select/main_test.go
+++ document-selection/select/main_test.go
@@ -6,15 +6,15 @@ import (
 
 func TestCorrectResponseFromHandler(t *testing.T) {
 	request := Request{
-		QueryStringParameters: map[string]string{
-			"filter": "test",
+		PathParameters: map[string]string{
+			"documentId": "1",
 		},
 	}
 	response, err := Handler(request)
 	if err != nil {
 		t.Errorf("Error in request %s", err)
 	}
-	if response.Body != "{\"1\":\"Happy hacking with test\",\"2\":\"Another document test\"}" {
+	if response.Body != "{\"id\":1,\"title\":\"Test\",\"author\":\"Georg\"}" {
 		t.Errorf("error answer from the function: %s", response.Body)
 	}
 }
