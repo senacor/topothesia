f28a9a921e50ddd4dcf573c008d987bd44bc2ae3
Karpoukhine, Denis <Denis.Karpoukhine@senacor.com>
2020-11-05 12:30:29 +0100

    added import for FileTable and useState

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,6 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Input } from "antd";
+import { useState } from "react";
+
+import FileTable from "./components/FileTable";
 
 const { Search } = Input;
 
